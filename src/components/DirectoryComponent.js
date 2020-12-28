458687054796a5ddb25b894b6997cc76c162f3f5
Saad Zafar <saadzafar@Saads-Air.localdomain>
2020-12-28 18:35:42 -0500

    Review Structure Change

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -3,22 +3,11 @@ import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 import CampsiteInfo from './CampsiteInfoComponent'
 
 class Directory extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            selectedCampsite: null
-        };
-    }
-
-    conCampsiteSelect(campsite) {
-        this.setState({selectedCampsite: campsite});
-    }
-
     render() {
         const directory = this.props.campsites.map(campsite => {
             return (
                 <div key={campsite.id} className="col-md-5 m-1">
-                    <Card onClick={() => this.conCampsiteSelect(campsite)}>
+                    <Card onClick={() => this.props.onClick(campsite.id)}>
                         <CardImg width="100%" src={campsite.image} alt={campsite.name}/>
                         <CardImgOverlay>
                             <CardTitle>{campsite.name}</CardTitle>                    
@@ -33,7 +22,6 @@ class Directory extends Component {
                 <div className="row">
                     { directory }
                 </div>
-                <CampsiteInfo className="row" campsite={this.state.selectedCampsite} />
             </div>  
         );
     }
