90cca9ce4b393ea8ecdccb2c111640644c462fe2
Saad Zafar <saadzafar@Saads-Air.fios-router.home>
2021-01-02 01:12:27 -0500

    React Router Params

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,35 +1,38 @@
 import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
-function RenderDirectoryItem({campsite}) {
+function RenderDirectoryItem({ campsite }) {
     return (
         <Card>
-            <CardImg width="100%" src={campsite.image} alt={campsite.name} />
-            <CardImgOverlay>
-                <CardTitle>{campsite.name}</CardTitle>
-            </CardImgOverlay>
+            <Link to={`/directory/${campsite.id}`}>
+                <CardImg width="100%" src={campsite.image} alt={campsite.name} />
+                <CardImgOverlay>
+                    <CardTitle>{campsite.name}</CardTitle>
+                </CardImgOverlay>
+            </Link>
         </Card>
     );
 }
 
 function Directory(props) {
 
-        const directory = props.campsites.map(campsite => {
-            return (
-                <div key={campsite.id} className="col-md-5 m-1">
-                    <RenderDirectoryItem campsite={campsite} />
-                </div>
-            );
-        })
-
+    const directory = props.campsites.map(campsite => {
         return (
-            <div className="container">
-                <div className="row">
-                    {directory}
-                </div>
+            <div key={campsite.id} className="col-md-5 m-1">
+                <RenderDirectoryItem campsite={campsite} />
             </div>
         );
-    
+    })
+
+    return (
+        <div className="container">
+            <div className="row">
+                {directory}
+            </div>
+        </div>
+    );
+
 }
 
 export default Directory;
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,9 +2,13 @@ import React, { Component } from 'react';
 import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
 import { CAMPSITES } from '../shared/campsites';
+import { COMMENTS } from '../shared/comments';
+import { PARTNERS } from '../shared/partners';
+import { PROMOTIONS } from '../shared/promotions';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
+import Contact from './ContactComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
@@ -12,7 +16,9 @@ class Main extends Component {
         super(props);
         this.state = {
             campsites: CAMPSITES,
-            selectedCampsite: null
+            comments: COMMENTS,
+            partners: PARTNERS,
+            promotions: PROMOTIONS,
         };
     }
 
@@ -20,8 +26,22 @@ class Main extends Component {
 
         const HomePage = () => {
             return (
-                <Home />
+                <Home 
+                    campsite={this.state.campsites.filter(campsite => campsite.featured)[0]}
+                    promotion={this.state.promotions.filter(promotion => promotion.featured)[0]}
+                    partner={this.state.partners.filter(partner => partner.featured)[0]}
+                />
             );
+        }
+
+        const CampsiteWithId = ({match}) => {
+            return (
+                <CampsiteInfo 
+                    campsite={this.state.campsites.filter(campsite => campsite.id === 
+                    +match.params.campsiteId)[0]}
+                    comments={this.state.comments.filter(comment => comment.campsiteId ===
+                    +match.params.campsiteId)} />
+            )
         }
 
         return (
@@ -30,6 +50,8 @@ class Main extends Component {
                 <Switch>
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
+                    <Route path='/directory/:campsiteId' component={CampsiteWithId}/>
+                    <Route exact path='/contactus' component={Contact}/>
                     <Redirect to='/home' />
                 </Switch>
                 <Footer />
