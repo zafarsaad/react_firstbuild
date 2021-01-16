076560ada1011835e1289a69b69333172b1eeac0
Saad Zafar <saadzafar@Saads-Air.fios-router.home>
2021-01-16 10:31:36 -0500

    to FrostByte

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -33,6 +33,8 @@ class CommentForm extends Component {
     }
 
     handleSubmit(values) {
+        this.toggleModal();
+        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
         console.log("Current state is =: " + JSON.stringify(values));
         alert("Current state is: " + JSON.stringify(values));
     }
@@ -97,7 +99,7 @@ class CommentForm extends Component {
 }
 
 
-function RenderComments({ comments }) {
+function RenderComments({ comments, addComment, campsiteId }) {
     if (comments) {
         return (
             <div className="col-md-5 m-1">
@@ -116,7 +118,7 @@ function RenderComments({ comments }) {
                         </div>
                     ))
                 }
-                <CommentForm />
+                <CommentForm campsiteId={campsiteId} addComment={addComment} />
             </div>
         )
     }
@@ -154,7 +156,10 @@ function CampsiteInfo(props) {
                 </div>
                 <div className="row">
                     <RenderCampsite campsite={props.campsite} />
-                    <RenderComments comments={props.comments} />
+                    <RenderComments 
+                        comments={props.comments} 
+                        addComment={props.addComment}
+                        campsiteId={props.campsite.id}/>
                 </div>
             </div>
         )
