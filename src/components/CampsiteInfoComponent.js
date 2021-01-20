4f4948a6280112f674bf8909e4176cd4ccd1ef50
Saad Zafar <saadzafar@Saads-Air.fios-router.home>
2021-01-19 19:20:31 -0500

    Fetch POST Comment

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -36,7 +36,7 @@ class CommentForm extends Component {
 
     handleSubmit(values) {
         this.toggleModal();
-        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+        this.props.postComment(this.props.campsiteId, values.rating, values.author, values.text);
         console.log("Current state is =: " + JSON.stringify(values));
         alert("Current state is: " + JSON.stringify(values));
     }
@@ -101,7 +101,7 @@ class CommentForm extends Component {
 }
 
 
-function RenderComments({ comments, addComment, campsiteId }) {
+function RenderComments({ comments, postComment, campsiteId }) {
     if (comments) {
         return (
             <div className="col-md-5 m-1">
@@ -120,7 +120,7 @@ function RenderComments({ comments, addComment, campsiteId }) {
                         </div>
                     ))
                 }
-                <CommentForm campsiteId={campsiteId} addComment={addComment} />
+                <CommentForm campsiteId={campsiteId} postComment={postComment} />
             </div>
         )
     }
@@ -178,7 +178,7 @@ function CampsiteInfo(props) {
                     <RenderCampsite campsite={props.campsite} />
                     <RenderComments 
                         comments={props.comments} 
-                        addComment={props.addComment}
+                        postComment={props.postComment}
                         campsiteId={props.campsite.id}/>
                 </div>
             </div>
--- src/redux/comments.js
+++ src/redux/comments.js
@@ -8,8 +8,10 @@ export const Comments = (state = { errMess: null, comments: [] }, action) => {
             return {...state, errMess: action.payload};
         case ActionTypes.ADD_COMMENT:
             const comment = action.payload;
-            comment.id = state.comments.length;
-            comment.date = new Date().toISOString();
+            // This below is not being done 1. JSON server assigned id
+            // 2. Adding date in the postComment method
+            // comment.id = state.comments.length;
+            // comment.date = new Date().toISOString();
             return {...state, comments: state.comments.concat(comment)};
         default:
             return state;
