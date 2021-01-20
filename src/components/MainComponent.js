78b0783148eaaa3fdaf264dab977061fc367135f
Saad Zafar <saadzafar@Saads-Air.fios-router.home>
2021-01-19 20:23:51 -0500

    React Animations 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -9,7 +9,8 @@ import About from './AboutComponent';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { actions } from 'react-redux-form';
-import { postComment, fetchCampsites, fetchComments, fetchPromotions } from '../redux/ActionCreators'
+import { postComment, fetchCampsites, fetchComments, fetchPromotions } from '../redux/ActionCreators';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 const mapStateToProps = state => {
     return {
@@ -40,7 +41,7 @@ class Main extends Component {
     render() {
         const HomePage = () => {
             return (
-                <Home 
+                <Home
                     campsite={this.props.campsites.campsites.filter(campsite => campsite.featured)[0]}
                     campsitesLoading={this.props.campsites.isLoading}
                     campsitesErrMess={this.props.campsites.errMess}
@@ -52,32 +53,36 @@ class Main extends Component {
             );
         }
 
-        const CampsiteWithId = ({match}) => {
+        const CampsiteWithId = ({ match }) => {
             return (
-                <CampsiteInfo 
-                    campsite={this.props.campsites.campsites.filter(campsite => campsite.id === 
-                    +match.params.campsiteId)[0]}
+                <CampsiteInfo
+                    campsite={this.props.campsites.campsites.filter(campsite => campsite.id ===
+                        +match.params.campsiteId)[0]}
                     isLoading={this.props.campsites.isLoading}
                     errMess={this.props.campsites.errMess}
                     comments={this.props.comments.comments.filter(comment => comment.campsiteId ===
-                    +match.params.campsiteId)}
-                    commentsErrMess={this.props.comments.errMess} 
+                        +match.params.campsiteId)}
+                    commentsErrMess={this.props.comments.errMess}
                     postComment={this.props.postComment}
-                    />
+                />
             )
         }
 
         return (
             <div>
                 <Header />
-                <Switch>
-                    <Route path='/home' component={HomePage} />
-                    <Route exact path='/directory' render={() => <Directory campsites={this.props.campsites} />} />
-                    <Route path='/directory/:campsiteId' component={CampsiteWithId}/>
-                    <Route path='/aboutus' render={() => <About partners={this.props.partners} />} />
-                    <Route exact path='/contactus' render={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />}/>
-                    <Redirect to='/home' />
-                </Switch>
+                <TransitionGroup>
+                    <CSSTransition key={this.props.location.key} classNames="page" timeout={300}>
+                        <Switch>
+                            <Route path='/home' component={HomePage} />
+                            <Route exact path='/directory' render={() => <Directory campsites={this.props.campsites} />} />
+                            <Route path='/directory/:campsiteId' component={CampsiteWithId} />
+                            <Route path='/aboutus' render={() => <About partners={this.props.partners} />} />
+                            <Route exact path='/contactus' render={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />} />
+                            <Redirect to='/home' />
+                        </Switch>
+                    </CSSTransition>
+                </TransitionGroup>
                 <Footer />
             </div>
         );
